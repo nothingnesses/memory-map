7177e8cc9536df6326eacc457925e740acaa9f32
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2025-10-18 16:23:03 +0100

    add unique constraint on location

--- memory-map-backend/migrations/V2__add_locations_table.sql
+++ memory-map-backend/migrations/V2__add_locations_table.sql
@@ -2,5 +2,5 @@ CREATE TABLE locations (
 	id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	-- '4326' is the Spatial Reference System Identifier (SRID) for WGS 84,
 	-- the standard for GPS and global coordinate systems.
-	location GEOGRAPHY(Point, 4326) NOT NULL
+	location GEOGRAPHY(Point, 4326) NOT NULL UNIQUE
 );
