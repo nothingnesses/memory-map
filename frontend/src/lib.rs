20fd84b42d49b8185fadeaf441ac4ffa47b9842f
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2025-11-01 19:51:08 +0000

    Format

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -12,23 +12,23 @@ use crate::pages::home::Home;
 /// An app router which renders the homepage and handles 404's
 #[component]
 pub fn App() -> impl IntoView {
-    // Provides context that manages stylesheets, titles, meta tags, etc.
-    provide_meta_context();
+	// Provides context that manages stylesheets, titles, meta tags, etc.
+	provide_meta_context();
 
-    view! {
-        <Html attr:lang="en" attr:dir="ltr" attr:data-theme="light" />
+	view! {
+		<Html attr:lang="en" attr:dir="ltr" attr:data-theme="light" />
 
-        // sets the document title
-        <Title text="Welcome to Leptos CSR" />
+		// sets the document title
+		<Title text="Welcome to Leptos CSR" />
 
-        // injects metadata in the <head> of the page
-        <Meta charset="UTF-8" />
-        <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		// injects metadata in the <head> of the page
+		<Meta charset="UTF-8" />
+		<Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <Router>
-            <Routes fallback=|| view! { NotFound }>
-                <Route path=path!("/") view=Home />
-            </Routes>
-        </Router>
-    }
+		<Router>
+			<Routes fallback=|| view! { NotFound }>
+				<Route path=path!("/") view=Home />
+			</Routes>
+		</Router>
+	}
 }
