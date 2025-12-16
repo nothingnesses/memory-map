01d4506fda7a582519af8aac8e260101b0292600
nothingnesses <18732253+nothingnesses@users.noreply.github.com>
2025-12-16 21:06:02 +0000

    WIP

--- frontend/src/graphql_queries.rs
+++ frontend/src/graphql_queries.rs
@@ -1,2 +1,3 @@
+pub mod s3_object_by_id;
 pub mod s3_objects;
 pub mod upsert_s3_object;
---
+++ frontend/src/graphql_queries/s3_object_by_id.rs
@@ -0,0 +1,31 @@
+use crate::{
+	graphql_queries::s3_object_by_id::s3_object_by_id_query::{
+		S3ObjectByIdQueryS3ObjectById as S3Object, Variables,
+	},
+	post_graphql,
+};
+use graphql_client::GraphQLQuery;
+use leptos::error::Error;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+	schema_path = "graphql/schema.json",
+	query_path = "graphql/s3ObjectById.graphql",
+	response_derives = "Clone,Debug"
+)]
+pub struct S3ObjectByIdQuery;
+
+impl S3ObjectByIdQuery {
+	// @todo Add better error-handling
+	pub async fn run(id: i64) -> Result<S3Object, Error> {
+		Ok(post_graphql::<S3ObjectByIdQuery, _>(
+			&reqwest::Client::new(),
+			"http://localhost:8000/",
+			Variables { id },
+		)
+		.await?
+		.data
+		.ok_or("Empty response".to_string())
+		.map(|response| response.s3_object_by_id)?)
+	}
+}
